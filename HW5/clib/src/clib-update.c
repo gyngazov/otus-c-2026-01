dcbf362cb2788a1839bb1773fe231c656185f88d
the <l.gyngazov@r77.center-inform.ru>
2026-03-06 17:44:36 +0300

    free

--- HW5/clib/src/clib-update.c
+++ HW5/clib/src/clib-update.c
@@ -366,6 +366,7 @@ int main(int argc, char *argv[]) {
   if (opts.prefix) {
     setenv("CLIB_PREFIX", opts.prefix, 1);
     setenv("PREFIX", opts.prefix, 1);
+    free(opts.prefix);
   }
 
   setenv("CLIB_FORCE", "1", 1);
