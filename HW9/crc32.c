02c35054c250491f0dfc2e25f8eae69556836aee
Leo <leo@cent.os>
2026-04-16 19:04:43 +0300

    shared mem

--- HW9/crc32.c
+++ HW9/crc32.c
@@ -4,8 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 
-
-const uint32_t crc32_tab[] = {
+static const uint32_t crc32_tab[] = {
 	0x00000000, 0x77073096, 0xee0e612c, 0x990951ba, 0x076dc419, 0x706af48f,
 	0xe963a535, 0x9e6495a3,	0x0edb8832, 0x79dcb8a4, 0xe0d5e91e, 0x97d2d988,
 	0x09b64c2b, 0x7eb17cbd, 0xe7b82d07, 0x90bf1d91, 0x1db71064, 0x6ab020f2,
@@ -51,11 +50,10 @@ const uint32_t crc32_tab[] = {
 	0xb40bbe37, 0xc30c8ea1, 0x5a05df1b, 0x2d02ef8d
 };
 
-uint32_t crc32(const void *buf, size_t size)
+uint32_t crc(const void *p, int page, uint32_t crc)
 {
-    const uint8_t *p = buf;
-    uint32_t crc = ~0U;
-    while (size--)
-        crc = crc32_tab[(crc ^ *p++) & 0xFF] ^ (crc >> 8);
-    return crc ^ ~0U;
+    const uint8_t *mp = (uint8_t *) p;
+    for (int j = 0; j < page; j++)
+        crc = crc32_tab[(crc ^ *mp++) & 0xFF] ^ (crc >> 8);
+    return crc;
 }
--- HW9/main.c
+++ HW9/main.c
@@ -1,78 +1,59 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <fcntl.h>
 #include <sys/mman.h>
-#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
 #include "crc32.h"
 
-#define BLOCK 256
+#define PAGE_SIZE 4096
 
-uint32_t crc_round(const void *p, long size, uint32_t crc);
+uint32_t crc_block(int page, int fd, long i, uint32_t cr);
 
-int main(int argc, char **argv)
+int main(int argc, char **argv) 
 {
     if (argc != 2) {
-        printf("usage: %s {filename}\n", argv[0]);
-        return -1;
+        printf("usage: %s {file for crc 32}", argv[0]);
+        return EXIT_FAILURE;
     }
-    struct stat stt;
-    stat(argv[1], &stt);
-    long file_size = stt.st_size;
-    printf("size: %ld\n", file_size);
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("cannot open file");
+        return(EXIT_FAILURE);
+    }
+    struct stat sb;
+    if (fstat(fd, &sb) == -1) {
+        perror("cannot stat file");
+        return(EXIT_FAILURE);
+    }
+    long sz = sb.st_size;
+    uint32_t cr = ~0U;
+    long i = 0;
 
-    int shm_fd = shm_open(argv[1], O_RDONLY | O_CREAT, 0664);
-    if (shm_fd == -1) {
-        perror("shm_open");
-        printf("%d\n", shm_fd);
-        return 1;
+    for (; i < sz - PAGE_SIZE; i += PAGE_SIZE)
+        cr = crc_block(PAGE_SIZE, fd, i, cr);
+    long tail = sz - i;
+    if (tail > 0) 
+        cr = crc_block(tail, fd, i, cr);
+    printf("file: %s crc32: %u\n", argv[1], cr ^ ~0U);
+    if (close(fd) == -1) {
+        perror("cannot close file");
+        return EXIT_FAILURE;
     }
-    printf("file opened\n");
-    void *p;
-    long i = 0, tail;
-    uint32_t crc = ~0U;
-
-    for (; i < file_size - BLOCK; i += BLOCK) {
-        p = mmap(0, BLOCK, PROT_READ, MAP_SHARED, shm_fd, i);
-        if (p == MAP_FAILED) {
-            perror("mmap");
-            close(shm_fd);
-            return 1;
-        }
-        printf("mmaped %ld\n", i);
-        crc = crc_round(p, BLOCK, crc);
-    }
-    tail = file_size - i;
-    if (tail > 0) {
-        p = mmap(0, tail, PROT_READ, MAP_SHARED, shm_fd, i);
-        if (p == MAP_FAILED) {
-            perror("mmap");
-            close(shm_fd);
-            return 1;
-        }
-        crc = crc_round(p, tail, crc);
-    }
-
-    if (munmap(p, file_size) == -1) {
-        perror("munmap");
-    }
-    close(shm_fd);
-    printf("crc32 = %d\n", crc ^ ~0U);
-
     return EXIT_SUCCESS;
 }
 
-uint32_t crc_round(const void *p, long size, uint32_t tmp)
+uint32_t crc_block(int page, int fd, long i, uint32_t cr)
 {
-    uint32_t crc = tmp;
-    const uint8_t *p8 = p;
-    for (long j = 0; j < size; j++) {
-        printf("crc %ld: %ld\n", j, crc);
-        printf("p: %d\n", *p8);
-        printf("i=%d\n", (crc ^ *p8) & 0xFF);
-        crc = crc32_tab[(crc ^ *p8++) & 0xFF] ^ (crc >> 8);
+    void *p = mmap(NULL, page, PROT_READ, MAP_SHARED, fd, i);
+    if (p == MAP_FAILED) {
+        perror("cannot mmap");
+        exit(EXIT_FAILURE);
     }
-    return crc;
+    cr = crc(p, page, cr);
+    if (munmap(p, page) == -1) {
+        perror("cannot unmap");
+        exit(EXIT_FAILURE);
+    }
+    return cr;
 }
-
