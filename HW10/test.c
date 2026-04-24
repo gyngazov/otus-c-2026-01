22e31c350835ab8cd16467a54b9206d20021409e
Leo <leo@cent.os>
2026-04-24 17:11:14 +0300

    test thr list

--- HW10/test.c
+++ HW10/test.c
@@ -2,46 +2,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <errno.h>
 
 struct Div {
-    int nn;
-    FILE **fp;
+    int n;
+    char **files;
 };
 
 struct Div *divide(const int threads_n, const char *dir_name, int *n);
+static int count_files(const char *dir_name);
+static char **get_files(int num_files, DIR *dir);
 
 void main(int argc, char **argv)
 {   
     int n;
-    struct Div *d = divide(12, "xs", &n);
+    struct Div *divs = divide(3, "xs", &n);
     
-    if (d == NULL)
+    if (divs == NULL)
         exit(EXIT_FAILURE);
-    for (int i = 0; i < n; i++)
-        printf("%d ", (d + i)->nn);
+    int len;
+    for (int i = 0; i < n; i++) {
+        len = divs[i].n;
+        printf("%d: n->%d fsl->", i, len);
+        for (int k = 0; k < len; k++)
+            printf("%s ", divs[i].files[k]);
+    }
         
 }
-
+// тредлист
+// распределить файлы по потокам
 struct Div *divide(const int threads_n, const char *dir_name, int *m)
 {
     DIR *dir;
     struct dirent *entry;
 
-    dir = opendir(dir_name);
-    if (dir == NULL) {
-        perror("Failed to open directory");
-        return 1;
-    }
-    int k = 0;
-    while (readdir(dir) != NULL)
-        k++;
-    if (k == 0)
+    int k = count_files(dir_name);
+    if (k == -1) {
+        printf("Ошибка чтения папки\n");
         return NULL;
-//         printf("Name: %s, Type: %d
-// ", entry->d_name, entry->d_type);
-    
-
-    
+    }
     const int base = k / threads_n;
     const int rest = k % threads_n;
     const int n = threads_n >= k ? k : threads_n;
@@ -51,17 +50,59 @@ struct Div *divide(const int threads_n, const char *dir_name, int *m)
         printf("Нет памяти\n");
         return NULL;
     }
+    dir = opendir(dir_name);
+    if (dir == NULL) {
+        perror("Папка не доступна");
+        return NULL;
+    }
+    readdir(dir);
+    readdir(dir);
     int i = 0;
     for (; i < rest; i++) {
-        k = base + 1;
-        while (k-- > 0) {
-            entry = readdir(dir);
-            
-        }
+        divs[i].n = base + 1;
+        divs[i].files = get_files(base + 1, dir);
     }
-        (divs + i)->nn = base + 1;
-    for (; i < n; i++)
-        (divs + i)->nn = base;
+    for (; i < n; i++) {
+        divs[i].n = base;
+        divs[i].files = get_files(base, dir);
+    }
+
     closedir(dir);
     return divs;
 }
+
+// штук файлов в папке
+static int count_files(const char *dir_name)
+{
+    DIR *dir;
+    dir = opendir(dir_name);
+    if (dir == NULL) {
+        perror("Папка не доступна");
+        return -1;
+    }
+    int k = 0;
+    readdir(dir); // .
+    readdir(dir); // ..
+    while (readdir(dir) != NULL)
+        k++;
+    closedir(dir);
+    if (k == 0) {
+        printf("Нет файлов.\n");
+        return -1;
+    }
+    return k;
+}
+
+// список файлов для потока
+static char **get_files(int num_files, DIR *dir)
+{
+    struct dirent *entry;
+    char **fls = malloc(num_files * sizeof(char *));
+    for (int j = 0; j < num_files; j++) {
+        entry = readdir(dir);
+        fls[j] = entry->d_name;
+    } 
+    return fls;
+}
+
+
