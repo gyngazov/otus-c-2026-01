d53aed35c7d8a498267c5fddb9d1c4d0da920351
gyngazov <l.gyngazov@yandex.ru>
2026-04-26 17:16:37 +0300

    time out

--- HW11/test.c
+++ HW11/test.c
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/time.h>
 
 #define PORT        23
 #define BUF_SIZE    16384
@@ -15,19 +16,52 @@
 #define CMD_SIZE    1024
 #define FIGLET      "figlet"
 #define FORMAT      "%s /%s %s\n\r"
+#define USAGE       "Usage: %s -f <font> -m <message>\n"
+#define TIMEOUT_SEC 5
 
 
 int main(int argc, char** argv)
 {
-    if(argc != 3){
-        printf("Usage: %s <font> <message>\n", argv[0]);
+    if (argc == 1) {
+        printf(USAGE, argv[0]); 
         exit(EXIT_FAILURE);
     }
+    char *font;
+    char *msg;
+    int opt;
+    extern char *optarg;
+    while((opt = getopt(argc, argv, ":f:m:")) != -1) { 
+        switch(opt) {  
+            case 'f': 
+                font = optarg;
+                break;
+            case 'm': 
+                msg = optarg;
+                break;
+            case ':': 
+            case 'h':
+            case '?': 
+            default:
+                printf(USAGE, argv[0]); 
+                exit(EXIT_FAILURE);
+        } 
+    }
+
     int sock_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sock_fd < 0){
         perror("socket");
         exit(errno);
     }
+
+    struct timeval timeout;
+    timeout.tv_sec = TIMEOUT_SEC;
+    timeout.tv_usec = 0;
+	if (setsockopt(sock_fd, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) < 0) {
+		perror("Не установлен таймаут");
+		close(sock_fd);
+        exit(errno);
+	}
+
     struct sockaddr_in sock_addr = {0};
     sock_addr.sin_family = AF_INET;
     sock_addr.sin_port = htons(PORT);
@@ -43,12 +77,17 @@ int main(int argc, char** argv)
         exit(errno);
     }
     char buffer[BUF_SIZE];
-    while (recv(sock_fd, buffer, BUF_SIZE - 1, 0) > 0)
-		if (strstr(buffer, ANSWER) != NULL)
+    while (recv(sock_fd, buffer, BUF_SIZE - 1, 0) > 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            printf("Таймаут\n");
+            exit(EXIT_FAILURE);
+        } else if (strstr(buffer, ANSWER) != NULL) {
 			break;
+        }
+    }
 
     char command[CMD_SIZE];
-	if (snprintf(command, CMD_SIZE, FORMAT, FIGLET, argv[1], argv[2]) < 0) {
+	if (snprintf(command, CMD_SIZE, FORMAT, FIGLET, font, msg) < 0) {
 		printf("Не формируется строка\n");
 		exit(EXIT_FAILURE);
 	}
@@ -61,6 +100,10 @@ int main(int argc, char** argv)
 
     int r;
 	while ((r = recv(sock_fd, buffer, BUF_SIZE, 0)) > 0) {
+        if (errno == EAGAIN || errno == EWOULDBLOCK) {
+            printf("Таймаут\n");
+            exit(EXIT_FAILURE);
+        }
         buffer[r] = '\0';
         if (strstr(buffer, "\n") != NULL)
             break;
