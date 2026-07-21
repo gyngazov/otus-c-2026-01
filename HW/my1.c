37cb2771625fb95454e045f8043c97cb33b6f2c1
Leo <l.gyngazov@yandex.ru>
2026-07-21 15:22:28 +0300

    test gut

--- HW/my1.c
+++ HW/my1.c
@@ -2,6 +2,7 @@
 #include <mysql/mysql.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define SELECT "select id, code, val from barcodes where id between ? and ?"
 
@@ -17,7 +18,6 @@ int main() {
     char *err_text;
     MYSQL_STMT *stmt;
     MYSQL_BIND params_bind[2];
-    MYSQL_BIND result_bind[3];
 
     // Инициализируем дескриптор
     conn = mysql_init(NULL);
@@ -27,7 +27,7 @@ int main() {
     }
 
     // Устанавливаем соединение с сервером
-    if (!mysql_real_connect(conn, "localhost", "xtr", "123", "bark", 3306, NULL, 0)) {
+    if (!mysql_real_connect(conn, "10.0.59.96", "xtr", "123", "bark", 3306, NULL, 0)) {
         err_text = "Error: failed to connect to database:";
         goto err;
     }
@@ -38,14 +38,24 @@ int main() {
         return 1;
     }
 
-    const long start = 123, last = 1234;
- 
+    unsigned long length1[2];
+    bool          is_null1[2];
+    bool          error1[2];
+    const long start = 10, last = 122;
+    memset(params_bind, 0, sizeof(params_bind));
     params_bind[0].buffer_type = MYSQL_TYPE_LONG;
     params_bind[0].buffer = (char *) &start;
+    params_bind[0].is_null = &is_null1[0];
+    params_bind[0].length = &length1[0];
+    params_bind[0].error = &error1[0];
+
     params_bind[1].buffer_type = MYSQL_TYPE_LONG;
     params_bind[1].buffer = (char *) &last;
+    params_bind[1].is_null = &is_null1[1];
+    params_bind[1].length = &length1[1];
+    params_bind[1].error = &error1[1];
 
-    if (mysql_stmt_bind_param(stmt, bind)) {
+    if (mysql_stmt_bind_param(stmt, params_bind)) {
         fprintf(stderr, "Ошибка привязки параметра: %s", mysql_stmt_error(stmt));
         return 1;
     } 
@@ -56,27 +66,36 @@ int main() {
         return 1;
     }
 
-    // Получение метаданных (количество столбцов)
-    if (mysql_stmt_result_metadata(stmt)) {
-        fprintf(stderr, "Ошибка получения метаданных
-");
-        return 1;
-    }
+    MYSQL_BIND result_bind[3];
+    int           id_data;
+    char          code_data[150];
+    char          val_data[255];
+    unsigned long length[3];
+    bool          is_null[3];
+    bool          error[3];
 
-    int id;
-    char *code, *val;
+    memset(result_bind, 0, sizeof(result_bind));
 
     result_bind[0].buffer_type = MYSQL_TYPE_LONG;
-    result_bind[0].buffer = (void *)&id;
-    result_bind[0].buffer_length = sizeof(id);
+    result_bind[0].buffer = (char *)&id_data;
+    result_bind[0].is_null = &is_null[0];
+    result_bind[0].length = &length[0];
+    result_bind[0].error = &error[0];
 
+/* STRING COLUMN */
     result_bind[1].buffer_type = MYSQL_TYPE_STRING;
-    result_bind[1].buffer = (void *)code;
+    result_bind[1].buffer = (char *)code_data;
     result_bind[1].buffer_length = 150;
+    result_bind[1].is_null = &is_null[1];
+    result_bind[1].length = &length[1];
+    result_bind[1].error = &error[1];
 
     result_bind[2].buffer_type = MYSQL_TYPE_STRING;
-    result_bind[2].buffer = (void *)val;
+    result_bind[2].buffer = (char *)val_data;
     result_bind[2].buffer_length = 255;
+    result_bind[2].is_null = &is_null[2];
+    result_bind[2].length = &length[2];
+    result_bind[2].error = &error[2];
 
     if (mysql_stmt_bind_result(stmt, result_bind)) {
         fprintf(stderr, "Ошибка привязки результата: %s", mysql_stmt_error(stmt));
@@ -84,13 +103,22 @@ int main() {
     }
 
     // Извлечение и вывод данных
-    while (mysql_stmt_fetch(stmt))
-        printf("%d %s %d\n", id, code, val);
+    int i = 11;
+    int ret;
+    while (i-- > 0) {
+        ret = mysql_stmt_fetch(stmt);
+        if (ret == 1 || ret == MYSQL_NO_DATA) {
+            printf("ret: %d\n", ret);
+            break;
+        }
+        printf("%d %s %s\n", id_data, code_data, val_data);
+    }
+        
 
     mysql_stmt_free_result(stmt);
     mysql_stmt_close(stmt);
 
-    int ret = EXIT_SUCCESS;
+    ret = EXIT_SUCCESS;
     goto ex;
 
 err:
